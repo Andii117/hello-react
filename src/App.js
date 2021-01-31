7bff54fd26f04ed6102182acb22144cf0abbd168
Harold jara <andresjara640@gmail.com>
2021-01-31 11:31:11 -0500

    Creamos y adicionamos un componente para la lista de comentarios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
 import "../src/global.css";
-import BadgeNew from '../src/pages/BadgeNew';
+//import BadgeNew from '../src/pages/BadgeNew';
+import Badges from '../src/pages/Badges';
 
 
 function App() {
   return (
     <div className="App">
-      <BadgeNew />
+      <Badges />
     </div>
   );
 }
