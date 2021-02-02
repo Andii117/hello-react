dd56f8315e47b0f9ef7cda2d8a7f93604309ff2c
Harold jara <andresjara640@gmail.com>
2021-02-01 19:15:58 -0500

    adiciona de API, creación de componente error y loading

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Badge from '../pages/Badges';
-import BadgeNew from '../pages/BadgeNew'
+import BadgeNew from '../pages/BadgeNew';
+import Layout from '../components/Layout';
+import NotFound from '../pages/NotFound';
 
 function Apps(){
     return (
         <BrowserRouter>
-            <Switch>
-                <Route exact path="/badges" component={Badge} />
-                <Route exact path="/badges/new" component={BadgeNew} />
-            </Switch>
+            <Layout>
+                <Switch>
+                    <Route exact path="/badges" component={Badge} />
+                    <Route exact path="/badges/new" component={BadgeNew} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Layout>
         </BrowserRouter>
     );
 }
